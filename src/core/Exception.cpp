cd2d27a48bab4a013bde5f620b88faae453680ca
Allison <alibellows@gmail.com>
2019-11-11 18:33:21 -0700

    run black

--- src/core/Exception.cpp
+++ src/core/Exception.cpp
@@ -13,49 +13,49 @@ using namespace std;
 
 // This is for exceptions thrown in our own code
 void Exception::getBacktrace() {
-  void *array[200];
-  size_t size;
-  char **strings;
-  size_t i;
+    void *array[200];
+    size_t size;
+    char **strings;
+    size_t i;
 
-  size = backtrace(array, 200);
-  strings = backtrace_symbols(array, size);
+    size = backtrace(array, 200);
+    strings = backtrace_symbols(array, size);
 
-  for (i = 0; i < size; i++)
-    m_backtrace.push_back(strings[i]);
+    for (i = 0; i < size; i++)
+        m_backtrace.push_back(strings[i]);
 
-  free(strings);
+    free(strings);
 }
 
 void Exception::printBacktrace() const {
-  for (const auto& i : m_backtrace) {
-    cout << i << endl;
-  }
+    for (const auto& i : m_backtrace) {
+        cout << i << endl;
+    }
 }
 
 // This is for printing a backtrace on a fatal signal.
 class Trap {
-  typedef void (*Handler)(int);
-public:
-  Trap(int sig, Handler handler)
-  {
-    signal(sig, handler);
-  }
+    typedef void (*Handler)(int);
+    public:
+    Trap(int sig, Handler handler)
+    {
+        signal(sig, handler);
+    }
 };
 
 void printBacktrace() {
-  void *array[200];
-  size_t size;
+    void *array[200];
+    size_t size;
 
-  size = backtrace(array, 200);
+    size = backtrace(array, 200);
 
-  backtrace_symbols_fd(array, size, 2);
+    backtrace_symbols_fd(array, size, 2);
 }
 
 static void crashHandler(int sig) {
-  cerr << "Error: signal " << sig << ":" << endl;
-  printBacktrace();
-  exit(1);
+    cerr << "Error: signal " << sig << ":" << endl;
+    printBacktrace();
+    exit(1);
 }
 
 // Uncomment the following line to get a backtrace on SIGSEGV in the CLI
@@ -63,28 +63,28 @@ static Trap sigsegv(SIGSEGV, crashHandler);
 
 // This is for intercepting exceptions thrown by the std library.
 namespace {
-  void * last_frames[20];
-  size_t last_size;
-  std::string exception_name;
+    void * last_frames[20];
+    size_t last_size;
+    std::string exception_name;
 
-  std::string demangle(const char *name) {
-    int status;
-    std::unique_ptr<char,void(*)(void*)> realname(abi::__cxa_demangle(name, 0, 0, &status), &std::free);
-    return status ? "failed" : &*realname;
-  }
+    std::string demangle(const char *name) {
+        int status;
+        std::unique_ptr<char,void(*)(void*)> realname(abi::__cxa_demangle(name, 0, 0, &status), &std::free);
+        return status ? "failed" : &*realname;
+    }
 }
 
 extern "C" {
-  void __cxa_throw(void *ex, std::type_info *info, void (*dest)(void *)) {
-    exception_name = demangle(reinterpret_cast<const std::type_info*>(info)->name());
-    last_size = backtrace(last_frames, sizeof last_frames/sizeof(void*));
+    void __cxa_throw(void *ex, std::type_info *info, void (*dest)(void *)) {
+        exception_name = demangle(reinterpret_cast<const std::type_info*>(info)->name());
+        last_size = backtrace(last_frames, sizeof last_frames/sizeof(void*));
 
-    static void (*const rethrow)(void*,void*,void(*)(void*)) = (void (*const)(void*,void*,void(*)(void*)))dlsym(RTLD_NEXT, "__cxa_throw");
-    rethrow(ex,info,dest);
-  }
+        static void (*const rethrow)(void*,void*,void(*)(void*)) = (void (*const)(void*,void*,void(*)(void*)))dlsym(RTLD_NEXT, "__cxa_throw");
+        rethrow(ex,info,dest);
+    }
 }
 
 void printInterceptedBacktrace() {
-  std::cerr << "Caught a: " << exception_name << std::endl;
-  backtrace_symbols_fd(last_frames, last_size, 2);
+    std::cerr << "Caught a: " << exception_name << std::endl;
+    backtrace_symbols_fd(last_frames, last_size, 2);
 }
--- test/unit/raycer/StateTest.cpp
+++ test/unit/raycer/StateTest.cpp
@@ -2,53 +2,53 @@
 #include "raycer/State.h"
 
 namespace StateTest {
-  using namespace ::testing;
-  using namespace raycer;
+    using namespace ::testing;
+    using namespace raycer;
 
-  TEST(State, ShouldInitialize) {
-    State state;
-    ASSERT_EQ(false, state.traceEvents);
-    ASSERT_EQ(0, state.maxRecursionDepth);
-  }
+    TEST(State, ShouldInitialize) {
+        State state;
+        ASSERT_EQ(false, state.traceEvents);
+        ASSERT_EQ(0, state.maxRecursionDepth);
+    }
 
-  TEST(State, ShouldNotTraceEventsByDefault) {
-    State state;
-    state.recordEvent(nullptr, "foo");
-    ASSERT_EQ(nullptr, state.events.get());
-  }
+    TEST(State, ShouldNotTraceEventsByDefault) {
+        State state;
+        state.recordEvent(nullptr, "foo");
+        ASSERT_EQ(nullptr, state.events.get());
+    }
 
-  TEST(State, ShouldStartTracing) {
-    State state;
-    state.startTrace();
-    state.recordEvent(nullptr, "foo");
-    ASSERT_EQ(1ul, state.events->size());
-  }
+    TEST(State, ShouldStartTracing) {
+        State state;
+        state.startTrace();
+        state.recordEvent(nullptr, "foo");
+        ASSERT_EQ(1ul, state.events->size());
+    }
 
-  TEST(State, ShouldRecurse) {
-    State state;
-    state.recurseIn();
-    ASSERT_EQ(1, state.recursionDepth);
-    state.recurseOut();
-    ASSERT_EQ(0, state.recursionDepth);
-  }
+    TEST(State, ShouldRecurse) {
+        State state;
+        state.recurseIn();
+        ASSERT_EQ(1, state.recursionDepth);
+        state.recurseOut();
+        ASSERT_EQ(0, state.recursionDepth);
+    }
 
-  TEST(State, ShouldCountRays) {
-    State state;
-    state.recurseIn();
-    ASSERT_EQ(1, state.numRays);
-    state.recurseIn();
-    ASSERT_EQ(2, state.numRays);
-  }
+    TEST(State, ShouldCountRays) {
+        State state;
+        state.recurseIn();
+        ASSERT_EQ(1, state.numRays);
+        state.recurseIn();
+        ASSERT_EQ(2, state.numRays);
+    }
 
-  TEST(State, ShouldRecordHit) {
-    State state;
-    state.hit(nullptr, "Box");
-    ASSERT_EQ(1, state.intersectionHits);
-  }
+    TEST(State, ShouldRecordHit) {
+        State state;
+        state.hit(nullptr, "Box");
+        ASSERT_EQ(1, state.intersectionHits);
+    }
 
-  TEST(State, ShouldRecordMiss) {
-    State state;
-    state.miss(nullptr, "Box");
-    ASSERT_EQ(1, state.intersectionMisses);
-  }
+    TEST(State, ShouldRecordMiss) {
+        State state;
+        state.miss(nullptr, "Box");
+        ASSERT_EQ(1, state.intersectionMisses);
+    }
 }
